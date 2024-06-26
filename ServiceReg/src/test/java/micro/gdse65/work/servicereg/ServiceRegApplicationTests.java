d19a8dac39c5050f73e29bd09c47a1307d1d382b
rayan <rayancooray123456@gmail.com>
2024-06-26 13:09:20 +0530

    Initial commit

---
+++ ServiceReg/src/test/java/micro/gdse65/work/servicereg/ServiceRegApplicationTests.java
@@ -0,0 +1,13 @@
+package micro.gdse65.work.servicereg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
